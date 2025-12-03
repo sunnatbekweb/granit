851f9bcebac61ea4143c8b380aea5ec3c8752348
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-12-03 20:18:14 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "./components/layout/Footer"
+import { Header } from "./components/layout/Header"
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	)
+}
+
+export default App
