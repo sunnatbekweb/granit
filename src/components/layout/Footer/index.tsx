22301cb4f571594afec00ec3d79e59d139bb4c2f
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-12-08 19:55:21 +0500

    footer update

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,99 @@
 export const Footer = () => {
 	return (
 		<footer>
-			<div className="container"></div>
+			<div className="container">
+				<div className="pt-16 pb-12 grid grid-cols-1 md:grid-cols-5 gap-5 text-black">
+					<div className="flex justify-center md:justify-start">
+						<a href={"/"}>
+							<img src={"/images/logo.png"} alt="Footer logo" />
+						</a>
+					</div>
+					<div className="flex justify-center">
+						<ul className="flex flex-col gap-6">
+							<li className="flex justify-center md:justify-start">
+								<a href={"#about"} className="text-center md:text-left">
+									О компании
+								</a>
+							</li>
+							<li className="flex justify-center md:justify-start">
+								<a href={"#products"} className="text-center md:text-left">
+									Портфолио
+								</a>
+							</li>
+							<li className="flex justify-center md:justify-start">
+								<a href={"#contact"} className="text-center md:text-left">
+									Контакты
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className="flex justify-center">
+						<ul className="font-medium flex flex-col gap-2">
+							<li data-aos="zoom-out">
+								<a href="tel:+998976000003">+998 (97) 600-00-03</a>
+							</li>
+							<li data-aos="zoom-out">
+								<a href="tel:+998339000090">+998 (33) 900-00-90</a>
+							</li>
+						</ul>
+					</div>
+					{/* <div className="flex justify-center">
+						<a
+							href="https://yandex.uz/web-maps/-/CLaRe2OF"
+							target="_blank"
+							data-aos="zoom-out"
+							className="text-center md:text-left"
+						>
+							{t("footer.address")}
+						</a>
+					</div> */}
+					{/* <div className="flex justify-center md:justify-end">
+						<ul className="flex flex-row md:flex-col xl:flex-row gap-6">
+							<li data-aos="zoom-out-left">
+								<a
+									href="https://facebook.com"
+									target="_blank"
+									className="footer_social"
+								>
+									<Facebook />
+								</a>
+							</li>
+							<li data-aos="zoom-out-left">
+								<a
+									href="https://www.instagram.com/kyariz.uz/"
+									target="_blank"
+									className="footer_social"
+								>
+									<Instagram />
+								</a>
+							</li>
+							<li data-aos="zoom-out-left">
+								<a
+									href="https://t.me"
+									target="_blank"
+									className="footer_social"
+								>
+									<Telegram />
+								</a>
+							</li>
+						</ul>
+					</div> */}
+				</div>
+			</div>
+			<div className="py-5 sm:py-8 border-t">
+				<div className="container relative flex items-center justify-between">
+					<div className="sm:absolute top-1/2 left-1/2 sm:-translate-1/2 text-sm sm:text-base">
+						&copy; {new Date().getFullYear()}{" "}
+						<a href="mailto:empireofpolymers@gmail.com">ООО "Мастер строй"</a>
+					</div>
+					<div className="sm:absolute top-1/2 right-0 sm:-translate-y-1/2 text-sm sm:text-base">
+						Разработано:{" "}
+						<a href="https://onesystem.uz" target="_blank">
+							<strong>onesystem</strong>
+						</a>
+					</div>
+				</div>
+			</div>
 		</footer>
 	)
 }
